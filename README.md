5e94bc9ea9aa6705336f73739cb0a811662ff94e
JohnJames Hodgins <johnjameshodgins@economist.com>
2022-11-21 18:22:20 +0000

    learnings

--- README.md
+++ README.md
@@ -12,3 +12,51 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `cdk deploy`      deploy this stack to your default AWS account/region
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+
+##
+
+Simple example how to use a functional url for a lambda
+
+## How to use
+
+deploy
+get the url
+
+This functional url is not taking a body event but a query string :
+
+append to the url name=.....
+e.g.
+with a get request
+https://a6ij7jaqbnbxaeo6sgcpisbojm0koiip.lambda-url.eu-west-2.on.aws/?name=JJ
+
+should return a 200 
+and if the name is not in the table then 
+'no one was greeted by that name'
+
+with a post
+
+https://a6ij7jaqbnbxaeo6sgcpisbojm0koiip.lambda-url.eu-west-2.on.aws/?name=JJ
+
+
+{
+  "name": "JJ",
+  "date": 1669054292759
+}
+and 200 result
+
+
+then a get with same name should return when the person was greeted
+"Was greeted on 21/11/2022"
+
+
+## useful links
+
+aws docs
+https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda_nodejs-readme.html
+https://docs.aws.amazon.com/lambda/latest/dg/lambda-typescript.html
+
+
+
+
+
+
--- lib/lambda-calculator-stack.ts
+++ lib/lambda-calculator-stack.ts
@@ -1,16 +1,43 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { CfnOutput, RemovalPolicy } from "aws-cdk-lib";
+import { FunctionUrlAuthType, Runtime } from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
 
 export class LambdaCalculatorStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const table = new Table(this, "HelloJJ", {
+      partitionKey: { name: "name", type: AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaCalculatorQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const dynamoLambda = new NodejsFunction(this, "dynamoLambdaHandlerJJ", {
+      runtime: Runtime.NODEJS_14_X,
+      //entry is basically where the typescript file is
+      entry: path.join(__dirname, "/../functions/function.ts"),
+      handler: "handler",
+      environment: {
+        HELLO_TABLE_NAME: table.tableName,
+      },
+    });
+
+    //grant permissions
+    table.grantReadWriteData(dynamoLambda);
+
+    const myFunctionUrl = dynamoLambda.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+      },
+    });
+
+    new CfnOutput(this, 'FunctionUrl', {
+      value: myFunctionUrl.url,
+    });
   }
 }
